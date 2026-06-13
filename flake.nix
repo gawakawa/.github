d74ca4bc131b87ca2859a86f0e9ba957eb0743d8
gawakawa <mashurakiryu@icloud.com>
2026-06-13 23:31:54 +0900

    :construction_worker: Add zizmor pre-commit hook

--- flake.nix
+++ flake.nix
@@ -60,6 +60,10 @@
             statix.enable = true;
             deadnix.enable = true;
             actionlint.enable = true;
+            zizmor = {
+              enable = true;
+              args = [ "--offline" ];
+            };
             workflow-timeout = {
               enable = true;
               name = "Check workflow timeout-minutes";
